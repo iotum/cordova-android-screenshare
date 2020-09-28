c813f5cde8e33e835ad15a1098051458608c8e67
Rex Chiu <rex@iotum.com>
2020-09-28 15:01:29 -0400

    copy bitmap instead of reusing the same one

--- src/android/com/iotum/cordovaandroidscreenshare/CordovaAndroidScreenshare.java
+++ src/android/com/iotum/cordovaandroidscreenshare/CordovaAndroidScreenshare.java
@@ -96,7 +96,7 @@ public class CordovaAndroidScreenshare extends CordovaPlugin {
 
           // Send first frame for startRecord callback
           if (mSendFirstFrame) {
-            mBitmapBuffer = bitmap;
+            mBitmapBuffer = bitmap.copy(Bitmap.Config.ARGB_8888, false);
             sendImage();
           }
         }

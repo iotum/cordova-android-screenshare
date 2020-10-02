57227ffd6f4de11de49d655db243f407889bbc6d
Jerry Hu <jerry@ptellp.com>
2020-10-02 16:15:01 -0400

    add fps and compression

--- www/index.js
+++ www/index.js
@@ -1,8 +1,22 @@
+const MODULE_NAME = 'CordovaAndroidScreenshare';
+
 module.exports = {
-  startScreenshare: (callback) => {
-    cordova.exec(response => { callback(null, response) }, error => callback(error), "CordovaAndroidScreenshare", 'startProjection', null)
+  startScreenshare: (callback, fps, compression) => {
+    cordova.exec(
+      response => callback(null, response),
+      error => callback(error),
+      MODULE_NAME,
+      'startProjection',
+      [fps, compression]
+    )
   },
   stopScreenshare: (callback) => {
-    cordova.exec(response => { callback(null, response) }, error => callback(error), "CordovaAndroidScreenshare", 'stopProjection', null)
+    cordova.exec(
+      response => callback(null, response),
+      error => callback(error),
+      MODULE_NAME,
+      'stopProjection',
+      null
+    )
   }
 }

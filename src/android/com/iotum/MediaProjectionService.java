5fecc4247cbb710ce47c4eb461d9e36230f4ce0d
Rex Chiu <rex@iotum.com>
2020-11-16 16:17:30 -0500

    maybe?

--- src/android/com/iotum/MediaProjectionService.java
+++ src/android/com/iotum/MediaProjectionService.java
@@ -31,7 +31,7 @@ public class MediaProjectionService extends Service {
 
             mNotificationId += 1;
             Notification notification = new NotificationCompat.Builder(context, "foreground.service.channel")
-                    .setSmallIcon(android.R.drawable.icon)
+                    .setSmallIcon(android.R.drawable.ic_launcher)
                     .setContentTitle("Sharing your screen")
                     .setContentText("Everything on screen will be shared.")
                     .setOngoing(true)
